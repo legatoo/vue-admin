f6bbe75b4a425dd666307bfdc8b2b9f4a9e8ed16
steven <yangyifan03@gmail.com>
2017-08-28 16:59:12 +0800

    build

--- src/main.js
+++ src/main.js
@@ -12,23 +12,23 @@ import routes from './routes'
 import 'font-awesome/css/font-awesome.min.css'
 
 
-Vue.use(ElementUI)
-Vue.use(VueRouter)
-Vue.use(Vuex)
+Vue.use(ElementUI);
+Vue.use(VueRouter);
+Vue.use(Vuex);
 
 //NProgress.configure({ showSpinner: false });
 
 const router = new VueRouter({
   routes
-})
+});
 
 router.beforeEach((to, from, next) => {
   //NProgress.start();
-  if (to.path == '/login') {
+  if (to.path === '/login') {
     sessionStorage.removeItem('user');
   }
   let user = JSON.parse(sessionStorage.getItem('user'));
-  if (!user && to.path != '/login') {
+  if (!user && to.path !== '/login') {
     next({ path: '/login' })
   } else {
     next()
@@ -46,5 +46,5 @@ new Vue({
   store,
   //components: { App }
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
 
